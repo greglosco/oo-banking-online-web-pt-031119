dcd8d4506dffec3c71610fe3ca7a88d14df51a67
Gregory Losco <gtlosco@gmail.com>
2019-04-30 02:30:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,8 +3,9 @@ class Transfer
   
   attr_accessor :sender, :recipient, :amount
   
-  def initialize(sender, recipient, amount)
+  def initialize(sender, receiver, amount)
    @sender = sender 
+   @receiver = receiver
   end
   
 end
